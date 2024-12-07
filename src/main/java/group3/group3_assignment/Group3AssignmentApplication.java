4d55a7410da1522d810d0f28e69bbf0e258bd86a
jianxun <jianxunbak@gmail.com>
2024-12-07 12:58:01 +0800

    basic springboot setup

---
+++ src/main/java/group3/group3_assignment/Group3AssignmentApplication.java
@@ -0,0 +1,13 @@
+package group3.group3_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group3AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group3AssignmentApplication.class, args);
+	}
+
+}
