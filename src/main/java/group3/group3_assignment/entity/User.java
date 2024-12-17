4d21be69027b6e8344fbe483993a1423e5adbac2
k3lly-z <keerismyname@gmail.com>
2024-12-17 23:39:00 +0800

    added validation for user

--- src/main/java/group3/group3_assignment/entity/User.java
+++ src/main/java/group3/group3_assignment/entity/User.java
@@ -12,6 +12,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -32,13 +35,19 @@ public class User {
     @Column(name = "id")
     private Integer id;
 
-    @Column(name = "username", unique = true)
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    @Column(name = "username", unique = true, nullable = false)
     private String username;
 
-    @Column(name = "email", unique = true)
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
 
-    @Column(name = "password")
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Column(name = "password", nullable = false)
     private String password;
 
     @JsonIgnoreProperties("user")
