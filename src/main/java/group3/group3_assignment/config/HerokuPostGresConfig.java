493055bef66b7123083d87f11fe12f2ff5dcd8bf
jianxun <jianxunbak@gmail.com>
2025-01-24 15:28:23 +0800

    deploy test

--- src/main/java/group3/group3_assignment/config/HerokuPostGresConfig.java
+++ src/main/java/group3/group3_assignment/config/HerokuPostGresConfig.java
@@ -1,29 +1,30 @@
-package group3.group3_assignment.config;
+// package group3.group3_assignment.config;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+// import java.net.URI;
+// import java.net.URISyntaxException;
 
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.apache.commons.dbcp2.BasicDataSource;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class HerokuPostGresConfig {
+// @Configuration
+// public class HerokuPostGresConfig {
 
-    @Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+// @Bean
+// public BasicDataSource dataSource() throws URISyntaxException {
+// URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
-                + "?sslmode=require";
+// String username = dbUri.getUserInfo().split(":")[0];
+// String password = dbUri.getUserInfo().split(":")[1];
+// String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort()
+// + dbUri.getPath()
+// + "?sslmode=require";
 
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+// BasicDataSource basicDataSource = new BasicDataSource();
+// basicDataSource.setUrl(dbUrl);
+// basicDataSource.setUsername(username);
+// basicDataSource.setPassword(password);
 
-        return basicDataSource;
-    }
-}
+// return basicDataSource;
+// }
+// }
